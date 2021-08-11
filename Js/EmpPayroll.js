f7e83ba38b195e530bc265e328b211da9fe75fe6
shreyasalanke <shreyasalanke97@gmail.com>
2021-08-11 21:57:46 +0530

    MODIFIED: Program to modify using getters and setters

--- Js/EmpPayroll.js
+++ Js/EmpPayroll.js
@@ -1,7 +1,10 @@
 class PayrollModel {
-    get name() {
-        return this._name;
+
+    get id (){return this._id;}
+    set id(_id){
+        this._id = id;
     }
+    get name() {return this._name;}
     set name(name) {
         let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
         if (nameRegex.test(name))
@@ -10,56 +13,36 @@ class PayrollModel {
             throw "Name is Incorrect";
         }
     }
-    get profile() {
-        return this._profile;
-    }
+    get profile() {return this._profile;}
     set profile(profile) {
         this._profile = profile;
     }
-    get gender() {
-        return this._gender;
-    }
+    get gender() {return this._gender;}
     set gender(gender) {
         this._gender = gender;
     }
-    get department() {
-        return this._department;
-    }
+    get department() {return this._department;}
     set department(department) {
         this._department = department;
     }
-    get salary() {
-        return this._salary;
-    }
+    get salary() {return this._salary;}
     set salary(salary) {
         this._salary = salary;
     }
 
-    get startDate() {
-        return this._startDate;
-    }
+    get startDate() {return this._startDate;}
     set startDate(startDate) {
-        if (startDate <= new Date())
-            this._startDate = startDate.toLocaleString(undefined, {
-                timeZone: 'Asia/Kolkata'
-            });
-        else {
-            throw "Invalid Start Date";
-        }
+        this._startDate = startDate; 
     }
-    get note() {
-        return this._notes;
-    }
+    get note() {return this._notes;}
     set note(note) {
         this._note = note;
     }
-    get id() {
-        return this._id;
-    }
-    set id(id) {
-        this._id = id;
-    }
+    
     toString() {
-        return "Id = " + this.id + "\nName = " + this.name + "\nSalary = " + this.salary + "\nGender = " + this.gender + " \nStartdate = " + this.startDate + "\nDepartments = " + this.department + "\nProfile = " + this.profile + "\nNote = " + this.note;
+        return "Id = " + this.id + "\nName = " + this.name + "\nSalary = " + this.salary + 
+        "\nGender = " + this.gender + " \nStartdate = " + this.startDate + 
+        "\nDepartments = " + this.department + "\nProfile = " + this.profile + 
+        "\nNote = " + this.note;
     }
 }
